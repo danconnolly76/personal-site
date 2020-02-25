5762d9050d28348a61892cd263fc2f65ffa9a8bb
Daniel Connolly <daniel.connolly@steadygo.digital>
2020-02-25 16:46:45 +0000

    Developed site added profile section

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-		<title>Daniel Connolly - Portfolio</title>
+		<title>Daniel Connolly - Online CV</title>
         <!-- Link for custom CSS -->
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <!-- meta value -->
@@ -10,12 +10,9 @@
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 		
-<head>
-
-<body>
-	
+</head>
+<body class="body-colour">
 <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-	<div class="container">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -24,21 +21,131 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Home</a>
       </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Profile</a>
+        </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">About</a>
+        <a class="nav-link" href="#">Experience</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Content</a>
+        <a class="nav-link" href="#">Contact</a>
       </li>
     </ul>
   </div>
-  </div>
 </nav>
-
-	   <div class="image-container">
-			<img src="images/header-image-edited.jpg" class="header-image" height="675">
-			<div class="centered-text"><h1>Daniel Connolly<br>Portfolio</h1></div>
-       </div>		
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100 carousel-image" src="images/fountain.jpg" alt="Fountain Town Centre Barnsley">
+            <div class="carousel-caption d-none d-md-block">
+                <h4>Fountains in Barnsley Town Centre</h4>
+             </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100 carousel-image" src="images/george-yard.jpg" alt="George Yard Barnsley">
+            <div class="carousel-caption d-none d-md-block">
+                <h4>George Yard Barnsley</h4>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100 carousel-image" src="images/town-hall.jpg" alt="Barnsley Town Hall">
+            <div class="carousel-caption d-none d-md-block">
+                <h4>Barnsley Town Hall</h4>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+     <div class="container">
+         <div class="row justify-content-center education-title">
+             <div class="profile-title">
+             <h2>Profile</h2>
+             </div>
+         </div>
+         <hr class="hr-length">
+         <div class="row justify-content-center">
+             <div class="col-md-8">
+                <p class="text-center">A Web Developer who works with a range of technologies and programming languages.</p>
+             </div>
+         </div>
+     </div>
+    <div class="container">
+         <div class="row">
+             <div class="col-md-4">
+                <h3>About me</h3>
+                 <p>I am a Junior Web Developer who is currently working for a company based in New Pudsey, Leeds.
+                    I enjoy the work I do but I also have a number of hobbies and interests. These
+                    include watching football the teams I follow are Barnsley Football Club and Manchester United. I follow these
+                    two teams by attending football matches and watching on television or media device. Other interests include watching
+                    England Rugby Union and Football teams. I also go to the local gym twice a week if possible with the aim on increasing my health and fitness.
+                 </p>
+             </div>
+             <div class="col-md-4 text-center pt-5">
+                <img class="profile-image" src="images/no-profile-image.jpg">
+             </div>
+             <div class="col-md-4">
+                 <h3>Details</h3>
+                 <strong>Name:</strong>
+                 <p>Daniel Connolly</p>
+                 <strong>Location:</strong>
+                 <p>Barnsley, South Yorkshire</p>
+             </div>
+         </div>
+        <hr class="hr-length">
+    </div>
+  <div class="container">
+      <div class="row justify-content-center">
+          <h2>Experience</h2>
+      </div>
+      <div class="row">
+          <div class="col-md-2 pb-3">
+              <h3>Education</h3>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-md-4">
+              <h4>University of Huddersfield</h4>
+              <p>Sept 2014 - June 2019</p>
+          </div>
+          <div class="col-md-8">
+              <strong>BSc (Hons) Combined Studies (Computing)</strong><br>
+              <strong>Grade: First</strong>
+              <p>A big part of this degree coures involved Java programming and this was to learn the princples of OOP programming.
+                 In addition to programming how to design software with UML constituted to part of this study. Implementing algorithms
+                 with Java was further programming that was carried out during this study period. Other aspects include Computational Mathematics,
+                 OOP programming with C++, Creating a Computer network, web programming with Laravel and Spring, Database design and querying with SQL,
+                 Systems design, designing information for the web and the structure and the implemnetion of computer lamguages.
+              </p>
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-md-4">
+              <h4>Northern College</h4>
+              <p>Sept 2013 - May 2014</p>
+          </div>
+          <div class="col-md-8">
+              <strong>HE Diploma in Computing</strong><br>
+              <strong>Grade: Merit</strong>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                  when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+                  remaining essentially unchanged.
+              </p>
+          </div>
+      </div>
+      <hr class="hr-length">
+  </div>
 
 </body>
 		<!-- jQuery library -->
@@ -47,7 +154,6 @@
 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 		<!-- Latest compiled JavaScript -->
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
 </html>
 
 
