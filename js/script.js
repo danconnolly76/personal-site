bcee1cbc2448797754f51edeba46e633456c22a7
Daniel Connolly <daniel.connolly@steadygo.digital>
2020-02-28 13:20:29 +0000

    Made navbar active when anchor tags selected

--- js/script.js
+++ js/script.js
@@ -12,3 +12,7 @@ $('a[href*="#"]').on('click', function(event) {
         });
     }
 });
+$('.navbar-nav .nav-link').click(function(){
+    $('.navbar-nav .nav-link').removeClass('active');
+    $(this).addClass('active');
+})
