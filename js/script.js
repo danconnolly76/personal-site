502baae36494fa29adfbec31a97fbeb7c2e55e22
Daniel Connolly <daniel.connolly@steadygo.digital>
2020-02-28 12:33:01 +0000

    Added some final aspect to site

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $('.carousel').carousel({
-    interval: 5000
+    interval: 3000
+});
+$('a[href*="#"]').on('click', function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 1200, function(){
+            window.location.hash = hash;
+        });
+    }
 });
