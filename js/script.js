ec2ab217afc2eaeaadd10d4adfe19cabe8b8bd0e
Daniel Connolly <daniel.connolly@steadygo.digital>
2020-06-09 22:32:40 +0100

    Added some changes.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 $('.carousel').carousel({
     interval: 3000
 });
-$('a[href*="#"]').on('click', function(event) {
-    if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 1200, function(){
-            window.location.hash = hash;
-        });
-    }
+
+$('.navbar a').on('click', function (e) {
+   if (this.hash !== '') {
+   e.preventDefault();
+     const hash = this.hash;
+    $('html, body')
+      .animate({
+         scrollTop: $(hash).offset().top - 150
+       },1200);
+   }
+ });
+
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
 });
+
 $('.navbar-nav .nav-link').click(function(){
     $('.navbar-nav .nav-link').removeClass('active');
     $(this).addClass('active');
